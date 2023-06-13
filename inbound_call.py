1a214afa262a5e26fee27d82e083b813c379e276
hrciastuff <135465428+hrciastuff@users.noreply.github.com>
2023-06-12 19:59:45 -0500

    inbound_call.py

--- inbound_call.py
+++ inbound_call.py
@@ -1,1 +1,19 @@
 
+import speech_recognition as sr
+import time
+import unidecode
+import sys
+
+pin = sys.argv[1]
+r = sr.Recognizer()
+
+with sr.AudioFile("/tmp/opt." + pin + ".wav") as source:
+    
+    audio = r.listen(source)
+    
+    try:
+        text = r.recognize_google(audio, language='us-US')
+        time.sleep(1.5)
+        print(unidecode.unidecode(text.replace('','')))
+    except:
+        print("i can´t understand")
